529543984cd58de34761fdfd0ed5187ee155f78d
Ehmad Saeed <justehmadsaeed@gmail.com>
2020-08-28 17:16:58 +0500

    📦 NEW: ADD order form

--- WholeSale-Management-System/order_form.Designer.cs
+++ WholeSale-Management-System/order_form.Designer.cs
@@ -28,23 +28,315 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.order_table = new System.Windows.Forms.DataGridView();
+            this.productID_Col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.back_button = new System.Windows.Forms.Button();
+            this.companiesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.wholeSale_DBDataSet = new WholeSale_Management_System.WholeSale_DBDataSet();
+            this.companiesTableAdapter = new WholeSale_Management_System.WholeSale_DBDataSetTableAdapters.companiesTableAdapter();
+            this.product_box = new System.Windows.Forms.ComboBox();
+            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.quantity_box = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.productTableAdapter = new WholeSale_Management_System.WholeSale_DBDataSetTableAdapters.productTableAdapter();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.total_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.order_table)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companiesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wholeSale_DBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quantity_box)).BeginInit();
             this.SuspendLayout();
+            // 
+            // delete_button
+            // 
+            this.delete_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(60)))), ((int)(((byte)(69)))));
+            this.delete_button.FlatAppearance.BorderSize = 0;
+            this.delete_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.delete_button.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delete_button.ForeColor = System.Drawing.Color.White;
+            this.delete_button.Location = new System.Drawing.Point(1053, 146);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(123, 44);
+            this.delete_button.TabIndex = 27;
+            this.delete_button.Text = "Delete";
+            this.delete_button.UseVisualStyleBackColor = false;
+            this.delete_button.Click += new System.EventHandler(this.delete_button_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(604, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 24);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Quantity";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(189, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Product";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(60)))), ((int)(((byte)(69)))));
+            this.label1.Location = new System.Drawing.Point(52, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 35);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Order";
+            // 
+            // order_table
+            // 
+            this.order_table.AllowUserToAddRows = false;
+            this.order_table.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(68)))));
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.order_table.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.order_table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.order_table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productID_Col,
+            this.price_col,
+            this.Quantity_col,
+            this.email_col});
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.order_table.DefaultCellStyle = dataGridViewCellStyle8;
+            this.order_table.Location = new System.Drawing.Point(191, 196);
+            this.order_table.Name = "order_table";
+            this.order_table.Size = new System.Drawing.Size(989, 323);
+            this.order_table.TabIndex = 31;
+            this.order_table.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.product_table_CellEnter);
+            // 
+            // productID_Col
+            // 
+            this.productID_Col.HeaderText = "Product";
+            this.productID_Col.Name = "productID_Col";
+            this.productID_Col.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.productID_Col.Width = 300;
+            // 
+            // price_col
+            // 
+            this.price_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.price_col.HeaderText = "Price/-";
+            this.price_col.Name = "price_col";
+            this.price_col.ReadOnly = true;
+            this.price_col.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Quantity_col
+            // 
+            this.Quantity_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Quantity_col.HeaderText = "Quantity";
+            this.Quantity_col.Name = "Quantity_col";
+            this.Quantity_col.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // email_col
+            // 
+            this.email_col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.email_col.HeaderText = "Total Price";
+            this.email_col.Name = "email_col";
+            this.email_col.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // back_button
+            // 
+            this.back_button.FlatAppearance.BorderSize = 2;
+            this.back_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.back_button.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.back_button.ForeColor = System.Drawing.Color.Transparent;
+            this.back_button.Location = new System.Drawing.Point(1075, 595);
+            this.back_button.Name = "back_button";
+            this.back_button.Size = new System.Drawing.Size(101, 35);
+            this.back_button.TabIndex = 28;
+            this.back_button.Text = "Back";
+            this.back_button.UseVisualStyleBackColor = true;
+            this.back_button.Click += new System.EventHandler(this.back_button_Click_1);
+            // 
+            // companiesBindingSource
+            // 
+            this.companiesBindingSource.DataMember = "companies";
+            this.companiesBindingSource.DataSource = this.wholeSale_DBDataSet;
+            // 
+            // wholeSale_DBDataSet
+            // 
+            this.wholeSale_DBDataSet.DataSetName = "WholeSale_DBDataSet";
+            this.wholeSale_DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // companiesTableAdapter
+            // 
+            this.companiesTableAdapter.ClearBeforeFill = true;
+            // 
+            // product_box
+            // 
+            this.product_box.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.product_box.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.product_box.DataSource = this.productBindingSource;
+            this.product_box.DisplayMember = "product_ID";
+            this.product_box.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.product_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.product_box.FormattingEnabled = true;
+            this.product_box.Location = new System.Drawing.Point(277, 155);
+            this.product_box.Name = "product_box";
+            this.product_box.Size = new System.Drawing.Size(308, 26);
+            this.product_box.TabIndex = 32;
+            this.product_box.ValueMember = "product_ID";
+            // 
+            // productBindingSource
+            // 
+            this.productBindingSource.DataMember = "product";
+            this.productBindingSource.DataSource = this.wholeSale_DBDataSet;
+            // 
+            // quantity_box
+            // 
+            this.quantity_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.quantity_box.Location = new System.Drawing.Point(695, 155);
+            this.quantity_box.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.quantity_box.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.quantity_box.Name = "quantity_box";
+            this.quantity_box.Size = new System.Drawing.Size(120, 26);
+            this.quantity_box.TabIndex = 33;
+            this.quantity_box.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.quantity_box.KeyUp += new System.Windows.Forms.KeyEventHandler(this.quantity_box_KeyUp);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(99)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(909, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 44);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // productTableAdapter
+            // 
+            this.productTableAdapter.ClearBeforeFill = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(809, 542);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 26);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "TOTAL BILL    :";
+            // 
+            // total_label
+            // 
+            this.total_label.AutoSize = true;
+            this.total_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.total_label.ForeColor = System.Drawing.Color.White;
+            this.total_label.Location = new System.Drawing.Point(990, 537);
+            this.total_label.Name = "total_label";
+            this.total_label.Size = new System.Drawing.Size(83, 31);
+            this.total_label.TabIndex = 36;
+            this.total_label.Text = "Rs: 0";
             // 
             // order_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(78)))));
-            this.ClientSize = new System.Drawing.Size(978, 521);
-            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(60)))), ((int)(((byte)(69)))));
+            this.ClientSize = new System.Drawing.Size(1370, 699);
+            this.Controls.Add(this.total_label);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.quantity_box);
+            this.Controls.Add(this.product_box);
+            this.Controls.Add(this.back_button);
+            this.Controls.Add(this.order_table);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.delete_button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Name = "order_form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Order";
+            this.Text = "Edit Product";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.edit_product_form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.order_table)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.companiesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wholeSale_DBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quantity_box)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button delete_button;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView order_table;
+        private System.Windows.Forms.Button back_button;
+        private WholeSale_DBDataSet wholeSale_DBDataSet;
+        private System.Windows.Forms.BindingSource companiesBindingSource;
+        private WholeSale_DBDataSetTableAdapters.companiesTableAdapter companiesTableAdapter;
+        private System.Windows.Forms.ComboBox product_box;
+        private System.Windows.Forms.NumericUpDown quantity_box;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource productBindingSource;
+        private WholeSale_DBDataSetTableAdapters.productTableAdapter productTableAdapter;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label total_label;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productID_Col;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price_col;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity_col;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email_col;
     }
 }
--- WholeSale-Management-System/order_form.cs
+++ WholeSale-Management-System/order_form.cs
@@ -1,20 +1,147 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace WholeSale_Management_System
 {
     public partial class order_form : Form
     {
+        SqlConnection con;
+        string empID;
+
         public order_form()
         {
             InitializeComponent();
+        }
+        public order_form(string emp, SqlConnection conArg)
+        {
+            con = conArg;
+            empID = emp;
+            InitializeComponent();
+        }
+
+        private void back_button_Click_1(object sender, EventArgs e)
+        {
+            new main_menu(empID, con).Show();
+            this.Close();
+        }
+        private void populateProduct()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from companies order by company_id", con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+
+            order_table.Rows.Clear();
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                string id = ds.Tables[0].Rows[i].ItemArray[0].ToString();
+                string supplier = ds.Tables[0].Rows[i].ItemArray[1].ToString();
+                string contact = ds.Tables[0].Rows[i].ItemArray[2].ToString();
+                string email = ds.Tables[0].Rows[i].ItemArray[3].ToString();
+
+                DataGridViewRow row1 = new DataGridViewRow();
+                row1.CreateCells(order_table);
+                row1.Cells[0].Value = id;
+                row1.Cells[1].Value = supplier;
+                row1.Cells[2].Value = contact;
+                row1.Cells[3].Value = email;
+                order_table.Rows.Add(row1);
+
+            }
+            con.Close();
+        }
+        private void addOrder()
+        {
+            if (product_box.Text.Length == 0) return;
+
+            foreach (DataGridViewRow row in order_table.Rows)
+            {
+                if (row.Cells[0].Value.ToString().Equals(product_box.Text))
+                {
+                    row.Cells[2].Value = quantity_box.Value;
+                    row.Cells[3].Value = float.Parse(row.Cells[1].Value.ToString()) * int.Parse(quantity_box.Text);
+                    calculateTotal();
+                    return;
+                }
+            }
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select price from product where product_ID = @productid", con);
+            cmd.Parameters.AddWithValue("@productid", product_box.Text);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            con.Close();
+            
+            if (ds.Tables[0].Rows.Count == 0) return;
+
+            float price = float.Parse(ds.Tables[0].Rows[0].ItemArray[0].ToString());
+            DataGridViewRow row1 = new DataGridViewRow();
+            row1.CreateCells(order_table);
+            row1.Cells[0].Value = product_box.Text;
+            row1.Cells[1].Value = price;
+            row1.Cells[2].Value = quantity_box.Value;
+            row1.Cells[3].Value = price * int.Parse(quantity_box.Text);
+            order_table.Rows.Add(row1);
+            calculateTotal();
+            product_box.Text = "";
+            quantity_box.Value = 1;
+            
+        }
+        private void product_table_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            DataGridViewRow row = order_table.Rows[rowIndex];
+
+            product_box.Text = row.Cells[0].Value.ToString();
+            quantity_box.Text = row.Cells[2].Value.ToString();
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            delete_record_from_database();
+        }
+
+        private void delete_record_from_database()
+        {
+            foreach (DataGridViewRow row in order_table.SelectedRows)
+            {
+                order_table.Rows.RemoveAt(row.Index);
+            }
+            calculateTotal();
+        }
+
+
+
+        private void edit_product_form_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'wholeSale_DBDataSet.product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this.wholeSale_DBDataSet.product);
+            // TODO: This line of code loads data into the 'wholeSale_DBDataSet.companies' table. You can move, or remove it, as needed.
+            this.companiesTableAdapter.Fill(this.wholeSale_DBDataSet.companies);
+
+        }
+        private void calculateTotal()
+        {
+            float total = 0;
+            foreach (DataGridViewRow row in order_table.Rows)
+            {
+                total += float.Parse(row.Cells[3].Value.ToString());
+            }
+            total_label.Text = "Rs: " + total;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addOrder();
+        }
+
+        private void quantity_box_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                addOrder();
         }
     }
 }
